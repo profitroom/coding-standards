39d6628a3510a7a69b170305e17bd4f578789b63
Andrzej Kupczyk <andrzej.kupczyk@profitroom.com>
2019-04-13 14:09:26 +0200

    ✨ Allow to create a config file using command

--- src/Command/ConfigurationCommand.php
+++ src/Command/ConfigurationCommand.php
@@ -3,18 +3,47 @@
 namespace Profitroom\CodingStandards\Command;
 
 use Composer\Command\BaseCommand;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Filesystem\Filesystem;
 
 class ConfigurationCommand extends BaseCommand
 {
+    /** @var string */
+    private $configFile;
+
+    /** @var \Profitroom\CodingStandards\Command\Filesystem */
+    private $filesystem;
+
+    public function __construct(string $name = null)
+    {
+        parent::__construct($name);
+
+        $this->configFile = getcwd() . '/.php_cs.dist';
+        $this->filesystem = new Filesystem;
+    }
+
     protected function configure()
     {
         $this->setName('cs:configuration')
-            ->setDescription('Create PhpCsFixer config file for the project');
+            ->setDescription('Create config file for the PHP Coding Style fixer')
+            ->addArgument('name', InputArgument::REQUIRED, 'FQCN of the configuration')
+            ->addOption('force', null, InputOption::VALUE_NONE, 'Forces operation even if the config already exists');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        if (!$input->getOption('force') && $this->filesystem->exists($this->configFile)) {
+            throw new \RuntimeException('Config file already exists');
+        }
+
+        $output->writeln('<info>Crafting config file for the project... 👷</info>');
+
+        $this->filesystem->dumpFile($this->configFile, sprintf(
+            "<?php return (new %s)->config();\n",
+            $input->getArgument('name')
+        ));
     }
 }
